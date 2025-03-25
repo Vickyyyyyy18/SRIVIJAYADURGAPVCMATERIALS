21fb129d9a9f0d0b5178b87c2d4347aaeb04f124
santhoshkumar <146054243+Vickyyyyyy18@users.noreply.github.com>
2025-03-25 15:52:55 +0530

    Update Gallery.tsx

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -26,10 +26,6 @@ export default function Gallery() {
       url: "https://images.unsplash.com/photo-1581092160607-ee22621dd758?auto=format&fit=crop&q=80",
       title: "Quality Products"
     }
-    {
-    url:"https://ibb.co/hxgPxqCN",
-    title:"chai"
-  }
   ];
 
   return (
